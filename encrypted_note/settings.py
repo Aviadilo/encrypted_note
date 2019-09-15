4f3080f23d016883a2976d0dfdf0da8ad7b35131
Hanna Lychauka <zuzga@tut.by>
2019-09-15 19:25:48 +0300

    user login, loguot, registration

--- encrypted_note/settings.py
+++ encrypted_note/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'notes',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL="/"
+
+LOGOUT_REDIRECT_URL="/"
