7fb6656f2395aa28a0c17205afb5cdbc83df0695
Hanna Lychauka <zuzga@tut.by>
2019-09-15 17:33:49 +0300

    Set the maxlength for password-form

--- notes/forms.py
+++ notes/forms.py
@@ -4,7 +4,7 @@ from .models import Note
 
 class NoteCreateForm(forms.ModelForm):
     category = forms.CharField(required=False)
-    password = forms.CharField(widget=forms.PasswordInput, label='key for encoding')
+    password = forms.CharField(widget=forms.PasswordInput, label='key for encoding', max_length=32)
 
     class Meta:
         model = Note
@@ -13,8 +13,9 @@ class NoteCreateForm(forms.ModelForm):
 
 class NoteUpdateForm(forms.ModelForm):
     category = forms.CharField(required=False)
-    password_for_decode = forms.CharField(widget=forms.PasswordInput, label='enter current key')
-    password_for_encode = forms.CharField(widget=forms.PasswordInput, label='enter new key, if you want to change it', required=False)
+    password_for_decode = forms.CharField(widget=forms.PasswordInput, label='enter current key', max_length=32)
+    password_for_encode = forms.CharField(widget=forms.PasswordInput, label='enter new key, if you want to change it',
+                                          required=False, max_length=32)
 
     class Meta:
         model = Note
